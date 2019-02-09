7fd083a46eaa334654adf92d39370d7d6fcdf9e1
Janisch Daniel <janischdaniel1@gmx.at>
2019-02-09 07:45:50 +0000

    deleted unnecessary code

--- application.py
+++ application.py
@@ -43,9 +43,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-#db = SQL(os.environ["DATABASE_URL"])
-
 # Set error to none
 error = None
 
@@ -72,78 +69,6 @@ def gallery():
 def achievements():
 
     return render_template("achievements.html")
-
-
-"""@app.route("/hub", methods=["GET", "POST"])
-@login_required
-def hub():
-    # Creating user events
-
-    if request.method == "POST":
-
-        # Define variables
-        date = request.form.get("date")
-        time = request.form.get("time")
-        game = request.form.get("game")
-        numberPlayers = request.form.get("numberPlayers")
-        eventNumber = request.form.get("eventNumber")
-
-        # Check if there are valid inputs for new event or joining existing event
-        if not eventNumber and not date and not time and not game and not numberPlayers:
-            error = "provide event number"
-            return render_template("hub.html", error=error)
-
-        # If user wants to join event (event Number is given)
-        # Query for event data
-        elif eventNumber:
-            playerLoggedIn = db.execute("SELECT username FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["username"]
-            playerTable = db.execute("SELECT number, player FROM players WHERE number = :eventNumber", eventNumber=eventNumber)
-            maxPlayer = db.execute("SELECT numberPlayers FROM event WHERE number = :eventNumber",
-                                   eventNumber=eventNumber)[0]["numberPlayers"]
-
-            # Chek if event is full
-            if (len(playerTable) >= maxPlayer):
-                error = "event already full"
-                return render_template("hub.html", error=error)
-
-            # Check if player is already in event
-            if any(d['player'] == playerLoggedIn for d in playerTable):
-                error = "you cannot join event again"
-                return render_template("hub.html", error=error)
-
-            # Insert player to table
-            db.execute("INSERT INTO players (number, player) VALUES (:number, :playerLoggedIn)",
-                       number=eventNumber, playerLoggedIn=playerLoggedIn)
-            return redirect("/")
-
-        # Else if user wants to create an event
-        elif date and time and game and numberPlayers:
-
-            # Query db for creator of event
-            creator = db.execute("SELECT username FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["username"]
-
-            # Insert data for a new event into db
-            db.execute("INSERT INTO event (creator, date, time, game, numberPlayers) VALUES (:creator, :date, :time, :game, :numberPlayers)",
-                       creator=creator, date=date, time=time, game=game, numberPlayers=numberPlayers)
-
-            # Query for newest event number
-            latestEventNumber = db.execute("SELECT number FROM event")[-1]["number"]
-
-            # Insert creator into players table
-            db.execute("INSERT INTO players (number, player) VALUES (:latestEventNumber, :creator)",
-                       latestEventNumber=latestEventNumber, creator=creator)
-
-            return redirect("/")
-
-    else:
-        # If user reaches page per redirect or GET request
-        # Query db for data
-        event = db.execute("SELECT e.number, e.creator, e.date, e.time, e.game, e.numberPlayers, \
-                           (SELECT string_agg(DISTINCT p.player, ',') FROM players p WHERE p.number = e.number) \
-                            AS players FROM event e ORDER BY e.number ASC")
-
-        return render_template("hub.html", event=event)"""
-
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
@@ -175,104 +100,6 @@ def contact():
 
     else:
         return render_template("contact.html")
-
-
-"""@app.route("/login", methods=["GET", "POST"])
-def login():
-    # Log user in
-
-    # Forget any user_id
-    session.clear()
-
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            error = "provide username"
-            return render_template("login.html", error=error)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            error = "must provide password"
-            return render_template("login.html", error=error)
-
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          username=request.form.get("username"))
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            error = "invalid username and/or password"
-            return render_template("login.html", error=error)
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
-        return redirect("/")
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
-
-
-@app.route("/logout")
-def logout():
-    #Log user out
-
-    # Forget any user_id
-    session.clear()
-
-    # Redirect user to login form
-    return redirect("/")
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    #Register user
-
-    if request.method == "POST":
-
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            error = "provide username"
-            return render_template("register.html", error=error)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            error = "provide password"
-            return render_template("register.html", error=error)
-
-        # Ensure password was confirmed
-        elif not request.form.get("confirmation"):
-            error = "confirm password"
-            return render_template("register.html", error=error)
-
-        # Ensure password and confirm password do match
-        elif request.form.get("password") != request.form.get("confirmation"):
-            error = "passwords do not match"
-            return render_template("register.html", error=error)
-
-        # Ensure username is not already taken
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
-                          username=request.form.get("username"))
-        if len(rows) == 1:
-            error = "username is already taken"
-            return render_template("register.html", error=error)
-
-        # Insert user to database and log him into session
-        #session["user_id"] = db.execute("INSERT INTO users (username, hash) VALUES(:username, :hash)", username=request.form.get
-                                        #("username"), hash=generate_password_hash(request.form.get("password")))
-
-        session["user_id"] = db.execute("INSERT INTO users (id, username, hash) VALUES ('3', 'hey', 'doodad')")
-
-        return redirect("/")
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("register.html")
-"""
 
 def errorhandler(e):
     """Handle error"""
